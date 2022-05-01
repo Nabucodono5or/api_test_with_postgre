85ca7dcb306e86c939083819d02c11076ee0e3f9
Jefferson Abreu <pereirajefferson06@gmail.com>
2022-05-01 14:26:10 -0300

    Criação do ambiente de desenvolvimento: completa

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
-import express from 'express';
+import express, { Express } from 'express';
 
-const app = express();
+const app:Express = express();
 
 app.listen(3000, () => console.log('server iniciado na porta 3000'));
