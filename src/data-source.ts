fc6bed57ae0e8a475e218d1dbc9de723e527bca2
Jefferson Abreu <pereirajefferson06@gmail.com>
2022-05-02 14:46:04 -0300

    criada conexao com banco de dados

---
+++ src/data-source.ts
@@ -0,0 +1,12 @@
+import { DataSource } from 'typeorm';
+
+const AppDataSource = new DataSource({
+    type: "postgres",
+    host: "localhost",
+    port: 5432,
+    username: "postgres",
+    password: "teste",
+    database: "Teste",
+})
+
+export default AppDataSource;
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,9 @@
 import express, { Express } from 'express';
 import "reflect-metadata";
+import AppDataSource from './data-source';
 
 const app:Express = express();
+
+AppDataSource.initialize().then(() => console.log("Conectado com êxito")).catch((err) => console.log("Error de conexão", err));
 
 app.listen(3000, () => console.log('server iniciado na porta 3000'));
