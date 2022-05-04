688dc94c9caf53a76f79037bc660f4a697d84b9d
Jefferson Abreu <pereirajefferson06@gmail.com>
2022-05-04 15:18:16 -0300

    migration: Teste de criação e remoção um sucesso

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -1,14 +1,16 @@
 import 'dotenv/config';
 import process from 'process';
 import { DataSource } from 'typeorm';
+import { CreateCategories1651598369597 } from './migrations/1651598369597-CreateCategories';
 
 const AppDataSource = new DataSource({
-    type: process.env.TYPEORM_CONNECTION,
+    type: "postgres",
     host: process.env.TYPEORM_HOST,
-    port: process.env.TYPEORM_PORT,
+    port: 5432,
     username: process.env.TYPEORM_USERNAME,
     password: process.env.TYPEORM_PASSWORD,
     database: process.env.TYPEORM_DATABASE,
+    migrations: [CreateCategories1651598369597]
 });
 
 export default AppDataSource;
